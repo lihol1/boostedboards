43d8af44bfd42ecd79fd876baf990caed8d8ace5
lihol1 <117180756+lihol1@users.noreply.github.com>
2024-01-19 14:33:08 +0300

    Add files via upload

---
+++ js/catalogAccessoires.js
@@ -0,0 +1,80 @@
+const accessories = [
+    {
+        id: 'acc1',
+        title: 'Boosted Belt Kit',
+        price: 19.00,
+        salePrice: 16.00,
+        qty: 1,
+        srcImg: './img/accessories/belt-kit/belt-kit.jpg',        
+        srcModal: './img/accessories/belt-kit/belt-kit-big.jpg',
+        srcImgWebp: './img/accessories/belt-kit/belt-kit-big.webp',
+        srcImgWebp2x: './img/accessories/belt-kit/belt-kit@2x.webp'
+    },
+    {
+        id: 'acc2',
+        title: 'Boosted Rev Stem Catch',
+        price: 29.56,
+        salePrice: 26.30,
+        qty: 1,
+        srcImg: './img/accessories/rev-stem-catch/rev-stem-catch.jpg',
+        srcModal: './img/accessories/rev-stem-catch/rev-stem-catch-big.jpg',
+        srcImgWebp: './img/accessories/rev-stem-catch/rev-stem-catch-big.webp',
+        srcImgWebp2x: './img/accessories/rev-stem-catch/rev-stem-catch@2x.webp'
+    },
+    {
+        id: 'acc3',
+        title: 'Bearing Service Kit',
+        price: 31.00,
+        salePrice: 27.00,
+        qty: 1,
+        srcImg: './img/accessories/bearing-service-kit/bearing-service-kit.jpg',
+        srcModal: './img/accessories/bearing-service-kit/bearing-service-kit-big.jpg',
+        srcImgWebp: './img/accessories/bearing-service-kit/bearing-service-kit-big.webp',
+        srcImgWebp2x: './img/accessories/bearing-service-kit/bearing-service-kit@2x.webp'
+    },
+    {
+        id: 'acc4',
+        title: 'Boosted Pulley / Belt Upgrade Kit',
+        price: 75.00,
+        salePrice: 73.25,
+        qty: 1,
+        srcImg: './img/accessories/belt-upgrade-kit/belt-upgrade-kit.jpg',
+        srcModal: './img/accessories/belt-upgrade-kit/belt-upgrade-kit-big.jpg',
+        srcImgWebp: './img/accessories/belt-upgrade-kit/belt-upgrade-kit-big.webp',
+        srcImgWebp2x: './img/accessories/belt-upgrade-kit/belt-upgrade-kit@2x.webp'
+    },
+    {
+        id: 'acc5',
+        title: 'Stomp Brake Fender',
+        price: 23.00,
+        salePrice: 18.00,
+        qty: 1,
+        srcImg: './img/accessories/stomp-brake-fender/stomp-brake-fender.jpg',
+        srcModal: './img/accessories/stomp-brake-fender/stomp-brake-fender-big.jpg',
+        srcImgWebp: './img/accessories/stomp-brake-fender/stomp-brake-fender-big.webp',
+        srcImgWebp2x: './img/accessories/stomp-brake-fender/stomp-brake-fender@2x.webp'
+    },
+    {
+        id: 'acc6',
+        title: 'Stomp Brake Fender',
+        price: 23.00,
+        salePrice: 18.00,
+        qty: 1,
+        srcImg: './img/accessories/rev-tube/rev-tube.jpg',
+        srcModal: './img/accessories/rev-tube/rev-tube-big.jpg',
+        srcImgWebp: './img/accessories/rev-tube/rev-tube-big.webp',
+        srcImgWebp2x: './img/accessories/rev-tube/rev-tube@2x.webp'
+    },
+    {
+        id: 'acc7',
+        title: 'Boosted Skid Plates',
+        price: 20.00,
+        salePrice: 18.50,
+        qty: 1,
+        srcImg: './img/accessories/skid-plates/skid-plates.jpg',
+        srcModal: './img/accessories/skid-plates/skid-plates-big.jpg',
+        srcImgWebp: './img/accessories/skid-plates/skid-plates-big.webp',
+        srcImgWebp2x: './img/accessories/skid-plates/skid-plates@2x.webp'
+    },
+
+]
